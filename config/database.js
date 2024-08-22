8a6c23b6642fa87e0c7ed46a4d6b0b0ff008647b
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-22 10:28:22 +0530

    removed flag

--- config/database.js
+++ config/database.js
@@ -8,10 +8,7 @@ require("dotenv").config();
 // connect to MongoDB
 
 const connectWithDb = () => {
-    mongoose.connect(process.env.DATABASE_URL,{
-        useNewUrlParser : true,
-        useUnifiedTopology: true
-    })
+    mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log("Connected to MongoDB"))
     .catch( (error) => {
         console.log("Error Connecting to MongoDB: " + error.message);
